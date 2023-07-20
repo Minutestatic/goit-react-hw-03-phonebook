18a7348c4655e9ef8a67ee2b6b9cbfab6b66d8ce
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-07-20 15:44:31 +0300

    Додайє зберігання контактів телефонної книги в localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,6 +13,22 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('phoneBookContacts'));
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(
+        'phoneBookContacts',
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
+
   handleFilterChange = event => {
     this.setState({ filter: event.target.value });
   };
